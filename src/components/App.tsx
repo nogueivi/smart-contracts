85f7bc87b4d921f7df287052e07b75a3e6fde0f7
Nogueira Vitor <rnogueira.vitor@gmail.com>
2021-04-17 22:19:14 +0200

    Add Material-UI for UI components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import TopBar from './navigation/TopBar';
+
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-
+        <TopBar />
     </div>
   )
 }
---
+++ src/components/navigation/LeftBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LeftBar = () => {
+    return (
+        <div>
+            This is the left bar
+        </div>
+    )
+}
+
+export default LeftBar;
